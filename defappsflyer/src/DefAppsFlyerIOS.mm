876f633facda5442f0d15188320423dc209094dc
Alexey Gulev <aleksey.gulev@gmail.com>
2018-02-24 19:53:02 +0300

    replace AppFlyerLib.framework with AppFlyerTracker framework

--- defappsflyer/src/DefAppsFlyerIOS.mm
+++ defappsflyer/src/DefAppsFlyerIOS.mm
@@ -2,7 +2,7 @@
 #include "DefAppsFlyer.h"
 #include <dmsdk/sdk.h>
 
-#include <AppsFlyerLib/AppsFlyerTracker.h>
+#include <AppsFlyerTracker/AppsFlyerTracker.h>
 
 void DefAppsFlyer_setAppsFlyerKey(const char*appsFlyerKey)
 {
