4947dcb9b2fed1d318bf871d3cc1b71c06701324
Alexey Gulev <aleksey.gulev@gmail.com>
2018-02-25 23:05:32 +0300

    added TrackEvent

--- defappsflyer/src/DefAppsFlyerAndroid.cpp
+++ defappsflyer/src/DefAppsFlyerAndroid.cpp
@@ -64,6 +64,7 @@ void DefAppsFlyer_setAppsFlyerKey(const char*appsFlyerKey)
   env->CallStaticVoidMethod(cls, method, dmGraphics::GetNativeAndroidActivity(), afkey);
 
   env->DeleteLocalRef(cls);
+  env->DeleteLocalRef(afkey);
 }
 
 void DefAppsFlyer_setIsDebug(bool is_debug)
@@ -73,18 +74,41 @@ void DefAppsFlyer_setIsDebug(bool is_debug)
   jclass cls = GetClass(env, JAR_PATH);
   jmethodID method = env->GetStaticMethodID(cls, "DefAppsFlyer_setIsDebug", "(Z)V");
   env->CallStaticVoidMethod(cls, method, is_debug ? JNI_TRUE : JNI_FALSE);
+
   env->DeleteLocalRef(cls);
 }
 
 void DefAppsFlyer_trackEvent(const char*eventName, dmArray<TrackData>* trackData)
 {
-  // NSString* key;
-  // NSString* value;
-  // TrackData data;
-  // for(uint32_t i = 0; i != trackData->Size(); i++)
-  // {
-  //   data = (*trackData)[i];
-  // }
+  AttachScope attachscope;
+  JNIEnv* env = attachscope.m_Env;
+  jclass hashMapClass = env->FindClass("java/util/HashMap");
+  jmethodID hashMapInit = env->GetMethodID(hashMapClass, "<init>", "(I)V");
+  jobject hashMapObj = env->NewObject(hashMapClass, hashMapInit, trackData->Size());
+  jmethodID hashMapId = env->GetMethodID(hashMapClass, "put","(Ljava/lang/Object;Ljava/lang/Object;)Ljava/lang/Object;");
+
+  TrackData data;
+  jstring key;
+  jstring value;
+  for(uint32_t i = 0; i != trackData->Size(); i++)
+  {
+    data = (*trackData)[i];
+    key = env->NewStringUTF(data.key);
+    value = env->NewStringUTF(data.value);
+    env->CallObjectMethod(hashMapObj, hashMapId, key, value);
+
+    env->DeleteLocalRef(key);
+    env->DeleteLocalRef(value);
+  }
+  jclass cls = GetClass(env, JAR_PATH);
+  jmethodID method = env->GetStaticMethodID(cls, "DefAppsFlyer_trackEvent","(Landroid/app/Activity;Ljava/lang/String;Ljava/util/Map;)V");
+  jstring jEventName = env->NewStringUTF(eventName);
+  env->CallStaticVoidMethod(cls, method, dmGraphics::GetNativeAndroidActivity(), jEventName, hashMapObj);
+
+  env->DeleteLocalRef(hashMapClass);
+  env->DeleteLocalRef(hashMapObj);
+  env->DeleteLocalRef(cls);
+  env->DeleteLocalRef(jEventName);
 }
 
 void DefAppsFlyer_trackAppLaunch()
