a0c7e1cdc5dd759a877787aa91acb8bfee204a4c
AGulev <me@agulev.com>
2022-11-02 16:49:38 +0100

    Updated to use dmScript::LuaToJson()

--- extension-appsflyer/src/appsflyer_callback.cpp
+++ extension-appsflyer/src/appsflyer_callback.cpp
@@ -34,36 +34,10 @@ static void InvokeCallback(MessageId type, const char* json)
     {
         return;
     }
+    
+    dmScript::JsonToLua(L, json, strlen(json)); // throws lua error if it fails
 
-    lua_pushnumber(L, type);
-    bool is_fail = false;
-    dmJson::Document doc;
-    dmJson::Result r = dmJson::Parse(json, &doc);
-    if (r == dmJson::RESULT_OK && doc.m_NodeCount > 0)
-    {
-        char error_str_out[128];
-        if (dmScript::JsonToLua(L, &doc, 0, error_str_out, sizeof(error_str_out)) < 0)
-        {
-            dmLogError("Failed converting object JSON to Lua: %s", error_str_out);
-            is_fail = true;
-        }
-    }
-    else
-    {
-        dmLogError("Failed to parse JSON object (%d): (%s)", r, json);
-        is_fail = true;
-    }
-    dmJson::Free(&doc);
-    if (is_fail)
-    {
-        lua_pop(L, 2);
-        assert(top == lua_gettop(L));
-        return;
-    }
-
-    int number_of_arguments = 3;
-
-    int ret = dmScript::PCall(L, number_of_arguments, 0);
+    int ret = dmScript::PCall(L, 3, 0);
     (void)ret;
     dmScript::TeardownCallback(m_luaCallback);
 
